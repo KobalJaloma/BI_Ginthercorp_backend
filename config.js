cffa10c2ef47fc97edc8378055e0b6d5a0c13ece
KobalJaloma <kirito10019@hotmail.com>
2023-07-18 09:47:31 -0600

    tercer commit

---
+++ config.js
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+//CONFIGURACION PARA SACAR EL DIRNAME
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
+dotenv.config({
+    path: path.resolve(_dirname, process.env.NODE_ENV + '.env')
+});
+
+console.log("ESTE ES EL ESPACIO ACTUAL " + process.env.NODE_ENV);
+
+export const config = {
+    NODE_ENV:process.env.NODE_ENV,
+
+    HOST:process.env.HOST,
+    PORT:process.env.PORT,
+
+    DB_ROUTE:process.env.DB_ROUTE,
+    DB_NAME:process.env.DB_NAME,
+    DB_USER:process.env.DB_USER, 
+    DB_PASSWORD:process.env.DB_PASSWORD
+};
